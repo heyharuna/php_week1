847459bce49fe2ff3976fa7d7282e73ddc6a5257
heyharuna <heyharuna0221@gmail.com>
2017-09-10 13:52:00 -0700

    create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+  </head>
+  <body>
+    <header>Week 1</header>
+  </body>
+</html>
